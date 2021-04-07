c6f5f5aa39ae428ad3807248d522950852cf509f
“samraarshad63git config --global user.email “samra.arshad@square63.org <YOUR@EMAIL.com>
2021-04-07 17:51:23 +0500

    added new gems, edited signup form new.html.erb, changed migration for creating users

--- Gemfile
+++ Gemfile
@@ -3,6 +3,9 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '2.6.3'
 
+gem 'rails-countries' 
+gem 'country_select', '~> 4.0'
+
 gem 'devise'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails', branch: 'main'
 gem 'rails', '~> 6.1.3', '>= 6.1.3.1'
--- db/migrate/20210407072302_devise_create_users.rb
+++ db/migrate/20210407072302_devise_create_users.rb
@@ -4,8 +4,14 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
+      t.string :email
+      t.string :encrypted_password 
+      t.string :firstname
+      t.string :lastname
+      t.string :country
+      t.string :contact
+      t.string :password
+
 
       ## Recoverable
       t.string   :reset_password_token
