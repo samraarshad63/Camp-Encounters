4e68e559c24692113a05687d225dc2f29bb5fed6
“samraarshad63git config --global user.email “samra.arshad@square63.org <YOUR@EMAIL.com>
2021-04-14 16:48:38 +0500

    File refactored

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,9 @@
 class UsersController < ApplicationController
-	before_action :authenticate_user!
+  before_action :authenticate_user!
 
-	def index; end
-	
-	def show
-		@user = User.find(params[:id])
-	end
+  def index; end
+
+  def show
+    @user = User.find(params[:id])
+  end
 end
