b9105065f7ac0f982dd2fd46ac87f53f957c4ccc
“samraarshad63git config --global user.email “samra.arshad@square63.org <YOUR@EMAIL.com>
2021-04-16 04:00:27 +0500

    Identation issue resolved

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,13 +1,13 @@
 class Users::SessionsController < Devise::SessionsController
-	def after_sign_in_path_for(current_user)
-		user_path(current_user)
-	end
+  def after_sign_in_path_for(current_user)
+    user_path(current_user)
+  end
 
-	def after_sign_up_path_for(current_user)
-		user_path(current_user)
-	end
+  def after_sign_up_path_for(current_user)
+    user_path(current_user)
+  end
 
-	def after_sign_out_path_for(current_user)
-		home_path
-	end
+  def after_sign_out_path_for(current_user)
+    home_path
+  end
 end
